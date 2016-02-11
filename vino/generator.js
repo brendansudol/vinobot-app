35ee9eac2e0fdb1f2d599c07fbc0c4c44faac5e3
brendan sudol <brendansudol@gmail.com>
2016-02-11 00:26:28 -0500

    start note generator

---
+++ vino/generator.js
@@ -0,0 +1,13 @@
+// Formula
+
+// (Intro) (Modifier) (Adjective) (Varietal).
+// (Adverb) (Adjective) (Flavor) flavors, (Adjective) (Flavor), and (Strength-word) (Flavor).
+// Drink (When) through (EndWhen).
+
+import {
+  intro,
+  modifier
+} from './words.js';
+
+console.log(intro);
+console.log(modifier);
---
+++ vino/words.js
@@ -0,0 +1,47 @@
+const intro = [
+  'foo'
+];
+
+const modifier = [
+  'baz'
+];
+
+const adjective = [
+  'foo'
+];
+
+const varietal = [
+  'foo'
+];
+
+const adverb = [
+  'foo'
+];
+
+const flavor = [
+  'foo'
+];
+
+const strength = [
+  'foo'
+];
+
+const when = [
+  'foo'
+];
+
+const when_end = [
+  'foo'
+];
+
+export {
+  intro,
+  modifier,
+  adjective,
+  varietal,
+  adverb,
+  flavor,
+  strength,
+  when,
+  when_end
+};
