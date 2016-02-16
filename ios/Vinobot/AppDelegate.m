b9a5ea06aad6ada0cfc997dca61c83a8e21716ea
brendan sudol <brendansudol@gmail.com>
2016-02-15 21:13:01 -0500

    add computer ip address to test out app on phone (commented in appdelegate.m)

--- ios/Vinobot/AppDelegate.m
+++ ios/Vinobot/AppDelegate.m
@@ -32,6 +32,7 @@
    */
 
   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+//  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.6:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
